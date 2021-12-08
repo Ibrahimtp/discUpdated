8a9fc478a0bb8c7765a77679ee3c9f5d1b82c761
Ibbalance <94036724+Ibbalance@users.noreply.github.com>
2021-12-08 21:56:33 +0100

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const user = require('../user.js') ;
+const user = require('../user.js');
 var session = require('express-session')
 
 /* GET home page. */
 router.get('/', async function(req, res) {
- /* if(req.session.userid) {
-    
+  /* if(req.session.userid) {
     currentUser = await user.findById(req.session.userid)
-    
     if(currentUser.status) {
       res.send("still working the your conclusion")
     }
@@ -16,67 +14,72 @@ router.get('/', async function(req, res) {
   res.render('discUpdated') }
   }
   else{
-  
   } */
-  res.render('welcome',{display:'d-none'});
+  res.render('welcome', {
+    display: 'd-none'
+  });
 });
 
-router.post("/signup",async(req,res)=>{
-  
-  
-  
-  
-  newUser = await new user({
-    name:capitalize(req.body.name.toLowerCase()),
-    date:req.body.date,
-    organization:req.body.org,
-    position:req.body.position
-    }) ;
-  
-  await newUser.save()
-  
-  session = req.session;
-      session.userid = newUser._id ;
+router.post("/signup", async(req, res)=> {
 
-  
-  
+
+
+
+  newUser = await new user({
+    name: capitalize(req.body.name.toLowerCase()),
+    date: req.body.date,
+    organization: req.body.org,
+    position: req.body.position
+  });
+
+  await newUser.save()
+
+  session = req.session;
+  session.userid = newUser._id;
+
+
+
   res.render('discUpdated')
 })
 
 
 
-router.post('/submitScores',async(req,res)=>{
-  if(req.session.userid) {
-    currentUser = await user.findByIdAndUpdate(req.session.userid,{most:req.body.most,least:req.body.least,status:true})
-    
+router.post('/submitScores', async(req, res)=> {
+  if (req.session.userid) {
+    currentUser = await user.findByIdAndUpdate(req.session.userid, {
+      most: req.body.most, least: req.body.least, status: true
+    })
+
     await currentUser.save()
-  //  res.send("Will get back to you with the conclusion")
-  res.render('getback')
-  } 
-  
-  else{
+    //  res.send("Will get back to you with the conclusion")
+    res.render('getback')
+  } else {
     res.redirect('/')
   }
 })
 
 
 
-router.get('/admin',async(req,res)=>{
-  users = await user.find({status:true})
-  
-  res.render('admin',{user})
- // res.send(users)
+router.get('/admin', async(req, res)=> {
+  users = await user.find({
+    status: true
+  })
+
+  res.render('admin', {
+    user
+  })
+  // res.send(users)
 })
 
-router.get('/d',async(req,res)=>{
+router.get('/dremove', async(req, res)=> {
   uh = await user.find().remove()
   res.send(uh)
 })
 
-router.get('/deleteR/:id',async(req,res)=>{
-  toBeDelete = await user.findById(req.params.id).remove() ;
+router.get('/deleteR/:id', async(req, res)=> {
+  toBeDelete = await user.findById(req.params.id).remove();
   res.redirect('/admin')
-}) 
+})
 
 
 
@@ -86,22 +89,20 @@ router.get('/deleteR/:id',async(req,res)=>{
 function capitalize(str) {
   const arr = str.split(" ");
 
-for (var i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
 
-}
+  }
 
 
-const str2 = arr.join(" ");
-return str2
+  const str2 = arr.join(" ");
+  return str2
 }
 
 function option(str) {
-  if(str=='option1') {
+  if (str == 'option1') {
     return 'male'
-  }
-  
-  else {
+  } else {
     return 'female'
   }
 }
